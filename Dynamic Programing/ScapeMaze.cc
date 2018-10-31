e91a2a13cdc70733149693d995fff8ca7e83bde5
oriolvelasco <oriolutilitats@gmail.com>
2018-10-31 21:54:30 +0100

    Create ScapeMaze.cc

---
+++ Dynamic Programing/ScapeMaze.cc
@@ -0,0 +1,58 @@
+// Nombre de camins per escapar d'una quadricula amb possibles caselles prohibides. 
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef vector<int> V;
+typedef vector<V> Taula;
+typedef vector<char> C;
+typedef vector<C> TaulaChar;
+
+
+Taula T;
+TaulaChar M;
+int n, m;
+
+
+
+int f(int i, int j) {
+  if(i < 0 or j < 0) return 0;
+
+  if(T[i][j] != -1) return T[i][j];
+  if (M[i][j] == 'X') {
+    T[i][j] = 0;
+    return T[i][j];
+  }
+
+  if (i == 0 and j == 0) {
+    T[i][j] = 1;
+    return T[i][j];
+  }
+
+  T[i][j] = f(i-1, j) + f(i, j-1);
+  if (T[i][j] >= 1e6) T[i][j] = 1e7;
+  return T[i][j];
+
+}
+
+
+int main() {
+  while(cin >> n >> m and (n > 0 or m > 0)) {
+    M = TaulaChar(n, C(m));
+    T = Taula(n, V(m, -1));
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) cin >> M[i][j];
+    }
+
+    int res = f(n-1, m-1);
+    if(res > 1e6) cout << "!!!" << endl; 
+    else cout << res << endl;
+
+  }
+
+  
+
+}
