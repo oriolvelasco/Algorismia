03d3aaedd5bc08eeb43a7ee1dbf339e7568a57fb
iflaki <44651996+iflaki@users.noreply.github.com>
2018-10-31 21:37:34 +0100

    Create Apagant els llums.cc

---
+++ Examens 2016 17/Apagant els llums.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void backtracking(int i, vector<bool>& v2, vector<int>& v, vector<vector<int> >& M, int& compt){
+    if (i == M.size()) {
+        for (int j = 0; j < v.size(); ++j) if (v[j] != 0) return;
+        ++compt;
+        return;
+    }
+    v2[i] = 0;
+    backtracking(i+1, v2, v, M, compt);
+    v2[i] = 1;
+    for (int k = 0; k < M[i].size(); ++k) v[M[i][k]] = (v[M[i][k]]+1)%2;
+    backtracking(i+1, v2, v, M, compt);
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<int> v(n);
+        for (int i = 0; i < n; ++i) cin >> v[i];
+        int m;
+        cin >> m;
+        vector<vector<int> > M(m);
+        for (int i = 0; i < m; ++i) {
+            int f;
+            cin >> f;
+            int x;
+            for (int j = 0; j < f; ++j) {
+                cin >> x;
+                M[i].push_back(x);
+            }
+        }
+        vector<bool> v2(m, 0);
+        int compt = 0;
+        backtracking(0, v2, v, M, compt);
+        cout << compt << endl;
+    }
+}
