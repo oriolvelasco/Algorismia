2ffeeaa176c15e97fd1ec5c4acd6256dd769a506
Andreu Huguet <andreuhuguet@gmail.com>
2018-11-01 23:24:32 +0100

    Més didàctic i llegible

--- Backtracking-Subsets/SumesIguals1.cc
+++ Backtracking-Subsets/SumesIguals1.cc
@@ -1,65 +1,42 @@
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
 
-typedef vector<int> Vec;
-typedef vector<string> SVec;
+using VE = vector<int>;
+using VB = vector<bool>;
 
-int s, n, suma;
-Vec V, posats;
+int n, S;
+VE nums;
+VB cont;
 
-void escriu() {
+void write_subset() {
+    bool first = true;
     cout << "{";
-    int j = 0;
-    bool primer = true;
-
-    while (posats[j] == 0 and j <= n) {
-        ++j;
-    }
-    
-    if(j >= n) {
-        cout << "}" << endl;
-        return;
-    }
-    
-    while(j < n) {
-        if(posats[j]) {
-            if(not primer) cout << ",";
-            primer = false;
-            cout << V[j];
+    for (int i = 0; i < n; ++i) {
+        if (cont[i]) {
+            if (first) first = false;
+            else cout << ",";
+			
+            cout << nums[i];
         }
-        ++j;
     }
-    cout << "}" << endl;
+    cout << "}";
+    cout << endl;
 }
 
-void f(int p) {
-	if (p == n and suma == s) {
-		escriu();
-		return;
-	}
+void rec(int s, int i, bool new_el) {
+    if (s == S and new_el) write_subset();
     
-    if(p==n) return;
-    
-    f(p+1);
-    
-    suma += V[p];
-    posats[p] = true;
-    f(p+1);
-    
-    suma -= V[p];
-    posats[p] = false;
-
+    if (i < n) {
+        cont[i] = true; rec(s + nums[i], i + 1, true);
+        cont[i] = false; rec(s, i + 1, false);
+    }
 }
 
 int main() {
-	cin >> s >> n;
-    V = Vec(n);
-    posats = Vec(n, false);
-    suma = 0;
-    
-    for(int i = 0; i < n; ++i) cin >> V[i];
-	
-	f(0);
+    cin >> S >> n;
+    nums = VE(n);
+    for (int& e : nums) cin >> e;
+    cont = VB(n);
+    rec(0, 0, true);
 }
