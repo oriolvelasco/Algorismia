7a6e6f42295fb967764f2ef0e9d362ad3ded8e81
Jennii <32710169+jennich@users.noreply.github.com>
2018-11-20 17:46:44 +0100

    Tresors en un mapa (1) Versió 2.cc
    Actualització 20/11/2018.
    La versió anterior estava malament: donava accepted als jocs de proves però amb uns altres podia entrar en un bucle!

--- Graph algorithms/Tresors en un mapa(1) Versio 2.cc
+++ Graph algorithms/Tresors en un mapa(1) Versio 2.cc
@@ -10,26 +10,40 @@ typedef vector<VC> VVC;
 VVC Map;
 typedef vector<int> VI;
 typedef vector<VI> VVI;
-VVI M;
+VVI VIS; //Array to indicate if a cell is already visited
 
-int f(int i, int j) {
-    if (i < 0 or i >= n or j < 0 or j >= m) return 0;
-    int& res = M[i][j];
-    if (res != -1) return res;
-    if (Map[i][j] == 't') return res = 1;
-    if (Map[i][j] == 'X') return res = 0;
-    res = max(f(i-1, j), f(i, j-1));
-    if (res) return res;
-    res = max(res, f(i+1, j));
-    if (res) return res;
-    res = max(res, f(i, j+1));
-    return res;
+/* IDEA:
+    If we code this problem as dynamic programming we can enter
+    into a cycle.
+    Example:
+    (n, m) = (2, 3), p = (i, j) = (1, 1); t = treasure
+    . . X
+    . p t
+    We return M[i][j] = f(i-1, j) or f(i+1, j);
+    First, we go up and if it is not possible (invalid i or there
+    is an obstacle), we go down
+    f(1, 1) == '.' ok, options i-1, i+1
+       --> f(0, 1) == '.' ok options i-1, i+1
+            . p X
+            . . t
+            --> f(-1, 1) not ok (invalid i)
+                __p__
+                . . X
+                . . t
+            --> f(1, 1) CYCLE!!
+*/
+bool f(int i, int j) {
+    if (i < 0 or i >= n or j < 0 or j >= m) return false;
+    if (VIS[i][j]) return false; //We have already visited this cell
+    VIS[i][j] = true;
+    if (Map[i][j] == 'X') return false;
+    return Map[i][j] == 't' or f(i-1, j) or f(i, j-1) or f(i+1, j) or f(i, j+1);
 }
 
 int main() {
     cin >> n >> m;
     Map = VVC(n, VC(m));
-    M = VVI(n, VI(m, -1));
+    VIS = VVI(n, VI(m, false));
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < m; ++j) cin >> Map[i][j];
     int r, c;
